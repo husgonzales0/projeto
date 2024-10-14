b385f0f1d6f998f97a681140c9eb8ef47c10704f
Hugo Gonzales <husgonzales@gmail.com>
2024-10-14 14:09:10 -0300

    adjust photo

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function togglemode() {
   if (html.classList.contains("light")) {
     // se tiver light mode, adicionar imagem light
     img.setAttribute("src", "./assets/avatar.png")
-    img.setAttribute("alt", "Foto do Hugo")
+    img.setAttribute("alt", "Hugo")
   } else {
     // se tiver sem light mode, manter a imagem normal
     img.setAttribute("src", "./assets/avatar.png")
